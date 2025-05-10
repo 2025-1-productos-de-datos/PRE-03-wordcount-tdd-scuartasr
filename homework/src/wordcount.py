c8bca2896d4a752af60c06d241f7d5d9f77085bd
Simón Cuartas Rendón <scuartasr@unal.edu.co>
2025-05-10 09:42:09 -0500

    Foto 1
    Definición de funciones de parse_args y read_all_lines

--- homework/src/wordcount.py
+++ homework/src/wordcount.py
@@ -1,1 +1,39 @@
+## wordcount.py
 
+import argparse
+import os
+
+
+def parse_args():
+
+    parser = argparse.ArgumentParser(description="Count words in files.")
+
+    parser.add_argument(
+        "input",
+        type=str,
+        help="Path to the input folder containing files to process",
+    )
+    parser.add_argument(
+        "output",
+        type=str,
+        help="Path to the output folder for results",
+    )
+
+    parsed_args = parser.parse_args()
+
+    return parsed_args.input, parsed_args.output
+
+
+def read_all_lines(input_folder):
+    lines = []
+    for filename in os.listdir(input_folder):
+        file_path = os.path.join(input_folder, filename)
+        with open(file_path, "r", encoding="utf-8") as f:
+            lines.extend(f.readlines())
+    return lines
+
+
+def main():
+    input_folder, output_folder = parse_args()
+
+    lines = read_all_lines(input_folder)
